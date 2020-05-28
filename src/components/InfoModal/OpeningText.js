8100e88bda76dca040f8dae3567e96e592cc4c0e
matildasutho <64000876+matildasutho@users.noreply.github.com>
2020-05-28 19:55:54 +1000

    Update OpeningText.js

--- src/components/InfoModal/OpeningText.js
+++ src/components/InfoModal/OpeningText.js
@@ -16,36 +16,34 @@ const OpeningText = () => {
         honour the oldest living culture on Earth.
       </p>
       <p>
-        This year's gallery has been curated and designed by Bachelor of
-        Environments student Cristina Margherita Napoleone and features the
-        works of staff, students and alumni who submitted images and artworks in
-        response to this year’s National Reconciliation Week theme in this
-        together.
+        This year's gallery has been curated and designed by Bachelor of 
+        Environments student Cristina Napoleone and features the works of staff, 
+        students and alumni who submitted images and artworks in response to this 
+        year’s National Reconciliation Week theme <i>in this together</i>. 
       </p>
       <p>
-        To be in this together is to not shy away from our grief, nor our
-        remorse. Art can help us grow through grief and amplify compassion that
-        compels us to love more and recognise a deeper kinship, toward one
+        To be <i>in this together</i> is to not shy away from our grief, nor 
+        our remorse. Art can help us grow through grief and amplify compassion 
+        that compels us to love more and recognise a deeper kinship, toward one 
         another and our shared more-than-human world.
       </p>
       <p>
-        This year marks the twentieth anniversary of the reconciliation walks of
-        2000, when people from all backgrounds came together to walk across the
-        nation and show their support for a more reconciled Australia. While
-        many steps have been taken, many steps remain as we learn our shared
-        story.
+        This year marks the twentieth anniversary of the reconciliation walks of 
+        2000, when people from all backgrounds came together to walk across the 
+        nation and show their support for a more reconciled Australia. While many 
+        steps have been taken, many steps remain as we learn our shared story. 
       </p>
       <p>
-        The gallery’s music plays <i>Biik Ngarrga</i> and{" "}
-        <i>Buln Buln Ngarrga</i> , written and performed by Mandy Nicholson live
-        with the Djirri Djirri Dancers at The University of Melbourne in April
-        2019. <i>Biik Ngarrga</i> is a Wurundjeri song used to teach wider
-        communities about the six interconnected layers of Country: Biik-ut
-        (Below Country), Biik-dui (On Country), Baanj Biik (Water Country),
-        Murnmut Biik (Wind Country), Wurru wurru Biik (Sky Country) and
-        Tharangalk Biik (Star Country). <i>Buln Buln Ngarrga</i> is also a
-        Wurundjeri song that honours the lyrebird, known for their expert
-        mimicry of surrounding sounds, both human and non-human.
+        GALLERY MUSIC
+        <i>Biik Ngarrga</i> and <i>Buln Buln Ngarrga</i>  were written and performed 
+        by Mandy Nicholson live with the Djirri Djirri Dancers at The University of 
+        Melbourne in April 2019. <i>Biik Ngarrga</i> is a Wurundjeri song used to 
+        teach wider communities about the six interconnected layers of Country: 
+        Biik-ut (Below Country), Biik-dui (On Country), Baanj Biik (Water Country), 
+        Murnmut Biik (Wind Country), Wurru wurru Biik (Sky Country) and Tharangalk 
+        Biik (Star Country). <i>Buln Buln Ngarrga</i> is also a Wurundjeri song that 
+        honours the lyrebird, known for their expert mimicry of surrounding sounds, 
+        both human and non-human. 
       </p>
     </div>
   );
